551bb84e4fe84b536f0232393da76db775b26ff4
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-07-11 22:16:22 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -17,12 +17,11 @@ function init() {
   let l = codes.length
 
   body.addEventListener('keydown', function(e) {
-    let key = e.key;
+    const key = e.key;
     
     if (key === codes[i]) {
       i++;
-      console.log(i);
-      
+
       if (i === l - 1) {
         alert('Yas!');
         i = 0;
